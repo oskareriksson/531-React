0c69ae892fb277cf48108ea0070ee07344a8d4d0
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-07 21:10:07 +0200

    Created a component called WeightCalc.js that will handle and serve the different weigth percentages for each exercise

---
+++ src/containers/WeightCalc.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class WeightCalc extends Component{
+  render(){
+    return(
+      <div className="weight-calc">
+
+      </div>
+    );
+  }
+}
+
+export default WeightCalc;
