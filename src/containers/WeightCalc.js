2d31ecfda5d0935bd0d3ecb6ca1a782ae8fadcbe
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-08 20:50:50 +0200

    Started working on a very basic table where the weight calculation will happen.
    Just a layout in progress for now

--- src/containers/WeightCalc.js
+++ src/containers/WeightCalc.js
@@ -1,10 +1,26 @@
 import React, {Component} from "react";
 
+//Stateful component
 class WeightCalc extends Component{
   render(){
     return(
-      <div className="weight-calc">
-
+      <div className="weight-calc container">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>
+              <div>
+                <h2>Maxlift</h2>
+                <form>
+                <input type="text" /><label>Maxlift in kilograms</label>
+                <input type="text" /><label>Repetitions that you can handle with that weight</label>
+                <input type="submit" value="Calculate" />
+                <input type="submit" value="Reset" />
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </div>
     );
   }
