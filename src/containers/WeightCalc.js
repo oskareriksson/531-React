d62e6b1e52a143093b2a87e28ee18059afc77460
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-09 13:26:21 +0200

    Rewrote the weightcalc form  using bootstrap grid system and forms, instead of table.

--- src/containers/WeightCalc.js
+++ src/containers/WeightCalc.js
@@ -5,22 +5,28 @@ class WeightCalc extends Component{
   render(){
     return(
       <div className="weight-calc container">
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td>
-              <div>
-                <h2>Maxlift</h2>
-                <form>
-                <input type="text" /><label>Maxlift in kilograms</label>
-                <input type="text" /><label>Repetitions that you can handle with that weight</label>
-                <input type="submit" value="Calculate" />
-                <input type="submit" value="Reset" />
-                </form>
+        <div className="row align-items-center">
+          <div className="col-12">
+            <h2>Maxlift</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="maxlift-in-kg">Maxlift in kilograms</label>
+                <input type="text" className="form-control" id="maxlift-in-kg" placeholder="Enter your current maxlift" />
               </div>
-            </td>
-          </tr>
-        </tbody>
+              <div className="form-group">
+                <label htmlFor="maxlift-reps">The amount of repetitions that you can handle with that weight</label>
+                <input type="text" className="form-control" id="maxlift-reps" placeholder="Amount of reps" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="one-rep-max">Your one rep max</label>
+                <input type="text" className="form-control" id="one-rep-max" placeholder="One rep max" aria-describedby="one-rep-max-info" />
+                <small id="one-rep-max-info" className="form-text text-muted" >If you already now your one rep max for a specific exercise, you only have to use this field in order to calculate your weights.</small>
+              </div>
+              <button type="submit" className="btn btn-secondary">Calculate</button>
+              <button type="submit" className="btn btn-secondary">Reset</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
