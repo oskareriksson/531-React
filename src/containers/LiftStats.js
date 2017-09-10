48d3a173a98d82b5280bb4d7c36e573257ff48c1
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-10 19:59:04 +0200

    Started working on the component that will store the users training stats

---
+++ src/containers/LiftStats.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class LiftStats extends Component {
+  render(){
+    return(
+      <div className="lift-stats container">
+        <div className="row align-items-center">
+          <div className="col-12">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LiftStats;
