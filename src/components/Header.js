7db8cbcd482631d39ca2450fdcbaa56dbbad23ee
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-11 19:33:24 +0200

    Added reactstrap column to Header component aswell

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Col} from "reactstrap";
 
 //Stateless component for now
 const Header = (props) => {
   return (
-    <div className="header">
+    <Col xs="12" className="header">
       <h1>Welcome to 531-React</h1>
       <p>This is going to be an application that will help you track you progress and also calculate the appropriate weights for the 5/3/1 strength program.</p>
-    </div>
+    </Col>
   );
 }
 
