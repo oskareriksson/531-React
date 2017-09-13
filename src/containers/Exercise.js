5dba1ad6e77f36d19f6063c1c650047151a90f5b
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-09-13 19:45:47 +0200

    Added radio inputs to Exercise.js

--- src/containers/Exercise.js
+++ src/containers/Exercise.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Col} from "reactstrap";
+import {Col, Form, FormGroup, Label, Input} from "reactstrap";
 
 //Stateful component
 class Exercise extends Component {
@@ -7,6 +7,28 @@ class Exercise extends Component {
     return(
       <Col xs="12" sm="6" md="3">
         <h2>{this.props.title}</h2>
+        <Form>
+          <FormGroup tag="fieldset">
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Cycle 1
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Cycle 2
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Cycle 3
+              </Label>
+            </FormGroup>
+          </FormGroup>
+        </Form>
       </Col>
     );
   }
